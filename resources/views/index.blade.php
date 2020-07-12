6b2ef4b65be438e513eb998994103291fcf12eac
motikan2010 <motikan2010@yahoo.co.jp>
2020-07-12 12:55:35 +0900

    add usage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-
+  <div class="card-deck box-shadow mb-3">
+    <div class="card mb-4 box-shadow">
+      <div class="card-header">
+      </div>
+      <div class="card-body">
+        <img src="{{ 'image/usage1.png' }}">
+        <img src="{{ 'image/usage2.png' }}">
+      </div>
+    </div>
+  </div>
 @endsection
